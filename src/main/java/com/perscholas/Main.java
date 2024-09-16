31c522ff99e367b241eafba0bd8582f3b557ce00
ronald.joyner-brown <r.brownjoyner@gmail.com>
2024-09-16 11:55:19 -0400

    Initial commit

---
+++ src/main/java/com/perscholas/Main.java
@@ -0,0 +1,19 @@
+package com.perscholas;
+
+public class Main {
+    public static void main(String[] args) {
+//        int a;
+//        int b;
+//        int sum;
+
+        for (int i = 1; i <= 12; i++) {
+            for (int j = 1; j <= 12; j++)
+            {
+                System.out.print((i * j) + "\t");//prints i * j the tab \t moves line by column
+            }
+            System.out.println();
+
+        }
+    }
+}
+//Hmm will revisit
